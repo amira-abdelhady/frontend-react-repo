60460d602716f1f5a0426a07c5dafc00a21e9dd4
amira-abdelhady <amiraabdelhady885@gmail.com>
2022-01-17 09:43:42 +0200

    initial commit or starting project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react"; 
+class App extends React.Component {
+  render() {
+    return (
+      <div className="test">
+        hello from react
+      </div>
+    );
+  }
+}
+
+export default App;
