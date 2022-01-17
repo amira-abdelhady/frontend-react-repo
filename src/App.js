ecdf0fa38710cb96ae5ed73d6066f9adf5dfbb22
amira-abdelhady <amiraabdelhady885@gmail.com>
2022-01-17 09:53:42 +0200

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import React from "react"; 
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <div className="test">
+      <Router>
+        <Navbar />
+        <Switch> 
+          
+        </Switch>
         <Footer />
-      </div>
+      </Router>
+    </div>
 
     );
   }
